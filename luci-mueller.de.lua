9709c9bfd777099c8237fb6c8a879b1b0843ff54
klml <m@klml.de>
2016-03-08 20:04:14 +0100

    ltm

---
+++ luci-mueller.de.lua
@@ -0,0 +1,9 @@
+-- File: luci-mueller.de.lua
+-- Zone: luci-mueller.de
+-- SOA record si automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "luci-mueller.de"
+
+-- A records
+a("klml", "95.143.172.147")
+redirect(_a, "http://klml.de")
