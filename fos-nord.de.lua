defdfa9f1d0941c17b085322a5e3f47497fdf26c
klml <m@klml.de>
2016-09-27 22:00:00 +0200

    fos-nord

---
+++ fos-nord.de.lua
@@ -0,0 +1,8 @@
+-- File: fos-nord.de.lua
+-- Zone: fos-nord.de
+-- SOA record si automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "fos-nord.de"
+
+-- A records
+redirect(_a, "http://www.fos-nord.musin.de/")
